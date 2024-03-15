d19df9fd83105bb67c5538c6f59cfb77df609a90
Kamve Gwijana <gwijgwij011@gmail.com>
2024-03-15 12:54:05 +0200

    simple notes taking, one note at a time

--- src/features/MyInputFeature.js
+++ src/features/MyInputFeature.js
@@ -1,41 +1,47 @@
 import React, { useState } from "react";
 import { TextInput } from "react-native-paper";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import { RoundedButton } from "../components/RoundedButton";
 import { MyList } from "./MyList";
 import { spacing } from "../utils/sizes";
+import { myColors } from "../utils/colors";
 
 export const MyInputFeature = () => {
   const [text, setText] = useState("");
+  const [item, setItem] = useState("none");
+
   return (
-    <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="What do you need a reminder for?"
-          onChangeText={(val) => {
-            setText(val);
-          }}
-          style={{}}
-        />
-        <View style={styles.buttonContainer}>
-          <RoundedButton
-            title="+"
-            size={40}
-            onPress={() => console.log({ text })}
+    <>
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholder="What do you need a reminder for?"
+            onChangeText={(val) => {
+              setText(val);
+            }}
+            style={{}}
           />
         </View>
+        <View style={styles.buttonContainer}>
+          <RoundedButton title="+" size={40} onPress={() => setItem(text)} />
+        </View>
       </View>
-    </View>
+      <View style={styles.itemList}>
+        <Text>{item}</Text>
+      </View>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "black",
+    flex: 0.2,
+    marginTop: spacing.xl,
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
   },
-  inputContainer: {},
-  buttonContainer: {},
+  inputContainer: { flex: 1, marginLeft: spacing.lg },
+  buttonContainer: { margin: spacing.lg },
+  itemList: { alignItems: "center" },
 });
